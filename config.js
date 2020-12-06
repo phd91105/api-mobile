98d4a14a3611067b0b0942275697b0ddac2c663e
Phạm Duy <PHD051199@gmail.com>
2020-12-06 13:30:33 +0700

    .

--- config.js
+++ config.js
@@ -4,26 +4,7 @@ const assert = require('assert');
 
 dotenv.config();
 
-// const {
-//     // PORT,
-//     // HOST,
-//     // HOST_URL,
-//     API_KEY,
-//     AUTH_DOMAIN,
-//     DATABASE_URL,
-//     PROJECT_ID,
-//     STORAGE_BUCKET,
-//     MESSAGING_SENDER_ID,
-//     APP_ID
-// } = process.env;
-
-// assert(PORT, 'PORT is required');
-// assert(HOST, 'HOST is required');
-
 module.exports = {
-    // port: PORT,
-    // host: HOST,
-    // url: HOST_URL,
     firebaseConfig: {
         apiKey: process.env.API_KEY,
         authDomain: process.env.AUTH_DOMAIN,
