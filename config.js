5f9431cd430dbd1398dc992a88702206a2e49a3b
Phạm Duy <PHD051199@gmail.com>
2020-12-06 11:51:40 +0700

    test

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ const assert = require('assert');
 
 dotenv.config();
 
-const {
-    // PORT,
-    // HOST,
-    // HOST_URL,
-    API_KEY,
-    AUTH_DOMAIN,
-    DATABASE_URL,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID
-} = process.env;
+// const {
+//     // PORT,
+//     // HOST,
+//     // HOST_URL,
+//     API_KEY,
+//     AUTH_DOMAIN,
+//     DATABASE_URL,
+//     PROJECT_ID,
+//     STORAGE_BUCKET,
+//     MESSAGING_SENDER_ID,
+//     APP_ID
+// } = process.env;
 
 // assert(PORT, 'PORT is required');
 // assert(HOST, 'HOST is required');
@@ -25,12 +25,12 @@ module.exports = {
     // host: HOST,
     // url: HOST_URL,
     firebaseConfig: {
-        apiKey: API_KEY,
-        authDomain: AUTH_DOMAIN,
-        databaseURL: DATABASE_URL,
-        projectId: PROJECT_ID,
-        storageBucket: STORAGE_BUCKET,
-        messagingSenderId: MESSAGING_SENDER_ID,
-        appId: APP_ID
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID
     }
 }
