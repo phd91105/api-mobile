d27ea84726055b7bbf5dd5881916454eef190559
Phạm Duy <PHD051199@gmail.com>
2020-12-06 11:36:29 +0700

    test

--- user_service.js
+++ user_service.js
@@ -1,10 +1,15 @@
 const firebase = require("firebase/app");
 require("firebase/auth");
-require('./config');
-const db = require("./db")
+// require('./config');
+// const db = require("./db")
+
+const apiKey = process.env.API_KEY;
+const fb = firebase.initializeApp({
+  apiKey: apiKey,
+});
 
 exports.addUser = (email, password) =>
-  db.auth().createUserWithEmailAndPassword(email, password);
+  fb.auth().createUserWithEmailAndPassword(email, password);
 
 exports.authenticate = (email, password) =>
-  db.auth().signInWithEmailAndPassword(email, password);
+  fb.auth().signInWithEmailAndPassword(email, password);
