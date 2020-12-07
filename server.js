dbf114e5f30653654e950848a88377b65b455879
Phạm Duy <PHD051199@gmail.com>
2020-12-07 21:58:58 +0700

    .

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const firebase = require("firebase");
 const noteRoutes = require('./routes/note-routes');
 app = express();
 
@@ -29,20 +28,31 @@ app.post("/api/signin", async (req, res) => {
   const { email, password } = req.body;
   try {
     var user = await userService.authenticate(email, password);
-    res.json(user);
+    accesstoken = user.user.ya;
+    refreshtoken = user.user.refreshToken;
+    res.status(201).json({ accessToken: accesstoken, refreshToken: refreshtoken });
+    // res.json(user);
   } catch (err) {
     res.status(401).json({ error: err.message });
   }
 });
 
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    // User is signed in.
-    app.use('/api', noteRoutes.routes);
-  } else {
-    // No user is signed in.
+app.use(function (req, res, next) {
+  if (req.headers && req.headers.authorization && String(req.headers.authorization.split(' ')[0]).toLowerCase() === 'bearer') {
+    var token = req.headers.authorization.split(' ')[1];
+    if (token == accesstoken) {
+      return next();
+    }
+    else {
+      return res.status(403).send({ message: 'Invalid Token' })
+    }
+  }
+  else {
+    return res.status(403).send({ message: 'Unauthorized' });
   }
 });
+
+app.use('/api', noteRoutes.routes);
 
 app.set("port", process.env.PORT);
 app.listen(app.get("port"), function () {
