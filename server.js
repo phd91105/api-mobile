0d46999c08c2d545b3b045a0bbe7a99cd60614c3
Phạm Duy <PHD051199@gmail.com>
2020-12-10 18:54:05 +0700

    .

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const jwt = require("jsonwebtoken");
 const firebase = require("firebase");
 
 const noteRoutes = require('./routes/note-routes');
@@ -17,10 +16,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send(`Server is listening at port ${app.get("port")}`);
 });
-
 /****************************************/
 app.use('/api', scheduleRoutes.routes);
-
 /****************************************/
 app.post("/api/signup", async (req, res) => {
   const { email, password } = req.body;
@@ -31,21 +28,17 @@ app.post("/api/signup", async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 });
-
 /****************************************/
 app.post("/api/signin", async (req, res) => {
   const { email, password } = req.body;
   try {
     var user = await userService.authenticate(email, password);
-    currentuser = user.user;
-    // var accesstoken = jwt.sign({ uid: user.user.uid, email: user.user.email, name: user.user.displayName, photoUrl: user.user.photoUrl }, 'secret-key', { algorithm: 'HS256', expiresIn: '1d' });
-    res.status(200).json({ email: currentuser.email, accessToken: currentuser.ya, refreshToken: currentuser.refreshToken });
-    // res.status(200).json(user);
+    currentuser = firebase.auth().currentUser;
+    res.status(200).json({ uid: currentuser.uid, email: currentuser.email, displayName: currentuser.displayName, photoURL: currentuser.photoURL, accessToken: currentuser.ya, refreshToken: currentuser.refreshToken });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
-
 /****************************************/
 app.post("/api/resetpass", async (req, res) => {
   const { email } = req.body;
@@ -56,7 +49,6 @@ app.post("/api/resetpass", async (req, res) => {
     res.status(401).json({ error: err.message });
   }
 });
-
 /********* middleware verify token *******/
 // app.use(function (req, res, next) {
 //   if (req.headers && req.headers.authorization && String(req.headers.authorization.split(' ')[0]).toLowerCase() === 'bearer') {
@@ -76,21 +68,65 @@ app.post("/api/resetpass", async (req, res) => {
 //     return res.status(403).send({ message: 'Unauthorized' })
 //   }
 // });
-
+/************* middleware ****************/
 app.use(function (req, res, next) {
-  uid = currentuser.uid;
-  next();
+  try {
+    uid = currentuser.uid;
+    next();
+  }
+  catch (e) {
+    res.send({ error: e.message });
+  }
 })
-
-/****************************************/
+/************ firebase auth ***************/
+firebase.auth().languageCode = 'vi';
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     app.use('/api', noteRoutes.routes);
+    app.post('/api/updateprofile', (req, res) => {
+      currentuser.updateProfile({
+        displayName: req.body.displayName,
+        photoURL: req.body.photoURL,
+      }).then(function () {
+        // Update successful.
+        res.send({ message: 'update profile successful', body: req.body })
+      }).catch(function (error) {
+        // An error happened.
+        res.send({ errror: error.message })
+      });
+    });
+    app.get('/api/getuserinfo', (req, res) => {
+      body = {
+        displayname: user.displayName,
+        email: user.email,
+        photoUrl: user.photoURL,
+        emailVerified: user.emailVerified,
+        uid: user.uid,
+      };
+      res.send(body);
+    });
+    app.post('/api/verifyemail', (req, res) => {
+      user.sendEmailVerification().then(function () {
+        // Email sent.
+        res.send({ message: 'verify email sent, check your inbox' })
+      }).catch(function (error) {
+        // An error happened.
+        res.send({ errror: error.message })
+      });
+    });
+    app.post('/api/changepass', (req, res) => {
+      user.updatePassword(req.body.password).then(function () {
+        // Update successful.
+        res.send({ message: 'password changed' });
+      }).catch(function (error) {
+        // An error happened.
+        res.send({ error: error.message });
+      });
+    });
   } else {
-
+    // console.log('error 1');
   }
 });
-
 /****************************************/
 app.set("port", process.env.PORT);
 app.listen(app.get("port"), function () {
