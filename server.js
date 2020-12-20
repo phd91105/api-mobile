95f61e90e9b91493b5349dbb1ca2b0f4580d16f6
Pham Duy <phd051199@gmail.com>
2020-12-20 18:16:36 +0700

    .

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.use(function (req, res, next) {
     if (user) {
       accessToken = user.ya;
       user = user;
+      uid = user.uid;
       next();
     } else {
       res.send({ error: "login first!" });
