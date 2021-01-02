bb701516bbbc856c09a65e5b73f7f4f5ed21be0b
Pham Duy <phd051199@gmail.com>
2021-01-02 09:14:43 +0700

    fix

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,18 +1,7 @@
 require("firebase/auth");
 require("../models/config");
 const admin = require("firebase-admin");
-const serviceAccount = {
-  "type": process.env.TYPE,
-  "project_id": process.env.PROJECT_ID,
-  "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
-  "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENT_ID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri": process.env.TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
-}
+const serviceAccount = require("./serviceAccount.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://api-node-c2241-default-rtdb.firebaseio.com",
